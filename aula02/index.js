148eda33d7e53fd48a530fc7a38ae4e5a70b2d77
oPatussi <opatussi@outlook.com>
2021-08-17 21:39:45 -0300

    Aula 17/08/2021

--- aula02/index.js
+++ aula02/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+
 //Rota principal
 app.get('/', (req,res) =>{
     res.send("Ta funfando")
+})
+
+//Rota Home
+app.get('/home', (req,res) =>{
+
+    const nome = req.query.nome;
+
+    res.send(`Seja bem vindo ${nome}!`)
+})
+
+//Rota Profile request param
+app.get('/profile/:username', (req,res) =>{
+
+    const username = req.params.username;
+
+    res.send(`Você está no perfil de ${username}!`)
+})
+
+//Rota Profile request body
+app.get('/login', (req,res) =>{
+
+    const user = req.body;
+    const {password,username} = req.body
+
+    console.log(user)
+
+    res.send(`Você está no perfil de ${user.username}!`)
 })
 
 app.listen(3000, () => {
---
+++ aula03/routes.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const routes = express.Router()
+
+//Dados
+const produtos = [
+    {id:1, nome:'suco de laranja',litros:0.5,preço: 3},
+    {id:2, nome:'suco de uva',litros:1,preço: 4},
+    {id:3, nome:'suco de abacaxi',litros:1.5,preço: 5}
+]
+
+routes.get('/produtos', (req,res) =>{
+    res.send(produtos)
+})
+
+routes.get('/produtos/:id', (req,res) =>{
+    const id = req.params.id
+    const index = id -1
+
+
+    res.send(produtos)
+})
+
+routes.get('/produtos/:id', (req,res) =>{
+    const produto = req.body
+
+    //Adicionar o produto referido pelo parametro ao vetor produtos
+    produtos.push(produto)
+
+    res.send(produtos)
+})
+
+
+module.exports = routes
